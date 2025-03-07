355dde3b54978d243e7b5a010c7447cf1c3baec3
GuigohC0D3 <guilhermewerneckpereira11@hotmail.com>
2025-03-07 00:58:07 -0300

    Inicio de  aula de JavaScript Alura

---
+++ exercicioJSAlura00/JS/script.js
@@ -0,0 +1,24 @@
+// Exercicio 1
+console.log("Estou estudando JavaScript!");
+
+// Exercicio 2
+const a = 5;
+
+const b = 10;
+
+console.log(b - a);
+
+console.log(a + b);
+
+// Exercicio 3
+const tecnologiaEscolhida = "Python";
+
+console.log("Estou estudando " + tecnologiaEscolhida, "!");
+
+// Exercicio 4
+console.log("Integracao realizada com sucesso!");
+
+// Exercicio 5
+const mudarTexto = document.querySelector("h1");
+
+mudarTexto.innerHTML = "Oi Jhon!";
---
+++ primeiroprojetoalura/JS/index.js
@@ -0,0 +1,13 @@
+// Exercicio 1
+console.log("Estou estudando JavaScript!");
+
+// Exercicio 2
+const a = 5;
+
+const b = 10;
+
+console.log(b - a);
+
+console.log(a + b);
+
+// Exercicio 3
